d6b0089af1e227890426ddf9c7168eeded6ff48b
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-05-13 18:50:51 +0200

    add event listene and update state

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import { Form } from "./components/Form/Form";
 import { TodoItem } from "./components/TodoItem/TodoItem";
+import { getSubheading } from "./utils/getSubheading";
 
 function App() {
-  const todos = [
+  const [isFormShown, setIsFormShown] = useState(false);
+  const [todos, setTodos] = useState([
     { name: "Zaplacic rachunki", done: false, id: 1 },
     { name: "Wyrzucic smieci", done: true, id: 2 },
-  ];
+  ]);
 
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div>
           <h1>Do zrobienia</h1>
-          <h2>5 zadan</h2>
+          <h2>{getSubheading(todos.length)}</h2>
         </div>
-        <button className={styles.button}>+</button>
+        {!isFormShown && (
+          <button
+            onClick={() => setIsFormShown(true)}
+            className={styles.button}
+          >
+            +
+          </button>
+        )}
       </header>
-      <Form />
+      {isFormShown && (
+        <Form
+          onFormSubmit={(newTodoName) => {
+            setTodos((prevTodos) => [
+              ...prevTodos,
+              { name: newTodoName, done: false, id: prevTodos.length + 1 },
+            ]);
+            setIsFormShown(false);
+          }}
+        />
+      )}
       <ul>
         {todos.map((todo) => (
           <TodoItem key={todo.id} name={todo.name} done={todo.done} />
